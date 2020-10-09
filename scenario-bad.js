8de69f712537135f62960dc9fb58f5fb15c1eb95
Cedric van Putten <me@bycedric.com>
2020-10-09 16:31:08 +0200

    chore: add scenarios to test

---
+++ scenario-bad.js
@@ -0,0 +1,24 @@
+const xcode = require('xcode');
+const path = require('path');
+
+const PBX_PROJECT_PATH = path.resolve('ios', 'testios2.xcodeproj', 'project.pbxproj');
+const SPLASH_SCREEN_PATH = path.join('testios2', 'SplashScreen.storyboard');
+
+console.log('Running test with the following paths:', {
+  PBX_PROJECT_PATH,
+  SPLASH_SCREEN_PATH,
+});
+
+// Create a new pbx project instance
+const project = xcode.project(PBX_PROJECT_PATH);
+// Now parse the project
+project.parseSync();
+
+// Determine if "SplashScreen.storyboard" exists
+const file = project.hasFile(SPLASH_SCREEN_PATH);
+
+if (file) {
+  console.log('Splash screen file located', file);
+} else {
+  throw new Errr('Splash Screen file does not exists');
+}
---
+++ scenario-good.js
@@ -0,0 +1,26 @@
+const xcode = require('xcode');
+const path = require('path');
+
+const PBX_PROJECT_PATH = path.resolve('ios', 'testios2.xcodeproj', 'project.pbxproj');
+const SPLASH_SCREEN_PATH = path.join('testios2', 'SplashScreen.storyboard')
+  // This workaround forces the paths to _always be_ a posix path, which is not the case on Windows ofc.
+  .replace(path.sep, path.posix.sep);
+
+console.log('Running test with the following paths:', {
+  PBX_PROJECT_PATH,
+  SPLASH_SCREEN_PATH,
+});
+
+// Create a new pbx project instance
+const project = xcode.project(PBX_PROJECT_PATH);
+// Now parse the project
+project.parseSync();
+
+// Determine if "SplashScreen.storyboard" exists
+const file = project.hasFile(SPLASH_SCREEN_PATH);
+
+if (file) {
+  console.log('Splash screen file located', file);
+} else {
+  throw new Errr('Splash Screen file does not exists');
+}
